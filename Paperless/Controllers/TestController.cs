c66edac87249a2cf043fc2cdf0492470d7aac785
CaZZe-dv <matthias.fichtinger7@gmail.com>
2024-09-19 17:31:36 +0200

    Projektdateien hinzufügen.

---
+++ Paperless/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Paperless.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Route("/")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Paperless project response" });
+        }
+    }
+}
