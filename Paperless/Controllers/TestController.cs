00e716b7e717e73bf39704515f39f5d26b3b2433
CaZZe-dv <matthias.fichtinger7@gmail.com>
2024-10-10 16:31:32 +0200

    Implement Sprint 2 features.

--- Paperless/Controllers/TestController.cs
+++ Paperless/Controllers/TestController.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Paperless.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Route("/")]
     public class TestController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+        // GET api/manage
+        [HttpGet("manage")]
+        public IActionResult ManageDocuments()
         {
-            return Ok(new { message = "Paperless project response" });
+            return Ok(new { message = "Manage: Dummy data returned" });
+        }
+
+        // POST api/upload
+        [HttpPost("upload")]
+        public IActionResult UploadDocument()
+        {
+            return Ok(new { message = "Upload: Document uploaded successfully (dummy response)" });
+        }
+
+        // DELETE api/delete
+        [HttpDelete("delete")]
+        public IActionResult DeleteDocument()
+        {
+            return Ok(new { message = "Delete: Document deleted successfully (dummy response)" });
         }
     }
 }
